3a10f44bd2f6512c879ca84b046af6a13fea0679
ankit.sin <ankit.sin@TEMP-ANKIT.local>
2018-10-11 10:11:29 +0530

    Nested struct in go

--- main.go
+++ main.go
@@ -4,12 +4,25 @@ import (
 	"fmt"
 )
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstname string
 	lastname  string
+	contact   contactInfo
 }
 
 func main() {
-	ankit := person{firstname: "Ankit", lastname: "Kumar"}
+	ankit := person{
+		firstname: "Ankit",
+		lastname:  "Kumar",
+		contact: contactInfo{
+			email:   "ankit@gmail.com",
+			zipCode: 1232,
+		},
+	}
 	fmt.Printf("%+v", ankit)
 }
